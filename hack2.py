4c8ab451e2de92469a8009a6221f5ed8b8fff01c
kantanand <kantanand.usk@gmail.com>
2023-07-20 16:05:59 +0530

    code clean up

--- hack2.py
+++ hack2.py
@@ -1,6 +1,6 @@
 import nltk
 # --------------------------------------------------------------------
-# Enable Below code to download nltk moduesl and commet back again 
+# Enable Below code to download nltk moduesl and commet back again
 # --------------------------------------------------------------------
 # import ssl
 # try:
@@ -19,7 +19,7 @@ from nltk.tokenize import word_tokenize
 import qg as qg
 import mysql.connector
 from OpenSSL import SSL
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, request, make_response
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
@@ -27,50 +27,52 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 def process(query):
-	query=query.lower()
-	tokens = nltk.word_tokenize(query)
-	tagged=nltk.pos_tag(tokens)
-	keywords=[]
-	numerals=[]
-	tags = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ', 'RB']
-	for i in tagged:
-			if i[1] in tags:
-				keywords.append(i[0])
-			elif i[1] == 'CD':
-				numerals.append(i[0])
-	sqlQuery = qg.queryGenerator(tagged,keywords,numerals,tokens)
-	print ("=======Answer for your question======")
-	print ("\n")
-	mydb = mysql.connector.connect(
-		host="localhost",
-		user="root",
-		password="root",
-		database="vistapoint_99_99_99", #vistapoint
-		port="1026"
-	)
-	mycursor = mydb.cursor()
-	mycursor.execute(sqlQuery)
-	myresult = mycursor.fetchall()
-	return myresult
+    query = query.lower()
+    tokens = nltk.word_tokenize(query)
+    tagged = nltk.pos_tag(tokens)
+    keywords = []
+    numerals = []
+    tags = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ', 'RB']
+    for i in tagged:
+        if i[1] in tags:
+            keywords.append(i[0])
+        elif i[1] == 'CD':
+            numerals.append(i[0])
+    sqlQuery = qg.queryGenerator(tagged, keywords, numerals, tokens)
+    print("=======Answer for your question======")
+    print("\n")
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="vistapoint_99_99_99",  # vistapoint
+        port="1026"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute(sqlQuery)
+    myresult = mycursor.fetchall()
+    return myresult
+
 
 @app.route('/generatedata', methods=['POST'])
-@cross_origin()
+# @cross_origin(origins=['localhost'])
 def handle_post():
     if request.method == 'POST':
         query = request.form['query']
         return process(query)
 
+
 @app.route('/test')
-@cross_origin()
+# @cross_origin(origins=['localhost'])
 def handle_get():
     return "test"
 
 @app.route('/nlp_query', methods=['POST'])
-@cross_origin()
+# @cross_origin(origins=['localhost'])
 def handel_nlp_query():
-    if request.method == 'POST':
-        query = request.form['query']
-        return process(query)
+	if request.method == 'POST':
+		query = request.form['query']
+		return process(query)
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0', port=5858, ssl_context='adhoc')
+    app.run(debug=True, host='0.0.0.0', port=5858, ssl_context='adhoc')
