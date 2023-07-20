3d566953d178b9a28c3524791fe4bd0d39868871
kantanand <kantanand.usk@gmail.com>
2023-07-20 12:08:17 +0530

    nltk download added

--- hack2.py
+++ hack2.py
@@ -1,4 +1,19 @@
 import nltk
+# --------------------------------------------------------------------
+# Enable Below code to download nltk moduesl and commet back again 
+# --------------------------------------------------------------------
+# import ssl
+# try:
+#     _create_unverified_https_context = ssl._create_unverified_context
+# except AttributeError:
+#     pass
+# else:
+#     ssl._create_default_https_context = _create_unverified_https_context
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('stopwords')
+# --------------------------------------------------------------------
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import qg as qg
